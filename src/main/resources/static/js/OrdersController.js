12be8ca8e6603a22f2b90ad5e0f457124810f7f2
estudiante <estudiante@linux-17.labinfo.is.escuelaing.edu.co>
2018-12-14 12:24:23 -0500

    fin

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -1,19 +1,23 @@
    var Company=(function(){
 
     var getEmpresa=function(empresa,tiempo,llama){
-    
+    alert(tiempo);
     axios.get('/info/'+JSON.stringify(empresa)+"/"+tiempo).then(function (response) {
             llama.Eraser();
             var fin=response.data;
+            var x = document.createElement("IMG");
             console.log(response.data);                
            
             document.getElementById("title").innerHTML += fin.Title;
-               document.getElementById("year").innerHTML += fin.Year;
-               document.getElementById("released").innerHTML += fin.Released;
-               document.getElementById("plot").innerHTML += fin.Plot;
-               document.getElementById("runtime").innerHTML += fin.Runtime;
-               
-   
+            document.getElementById("year").innerHTML += fin.Year;
+            document.getElementById("released").innerHTML += fin.Released;
+            document.getElementById("plot").innerHTML += fin.Plot;
+            document.getElementById("runtime").innerHTML += fin.Runtime;
+            x.setAttribute("src", fin.Poster);
+            x.setAttribute("width", "400");
+            x.setAttribute("height", "400");
+            x.setAttribute("alt", "FIN");   
+            document.body.appendChild(x);
         })
         .catch(function (error) {
             
